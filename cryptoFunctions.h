5f1546238813f54cd54bfdc14a2d568d9f0f91ed
Mark Meredith <mark.meredith@netl.doe.gov>
2022-02-11 15:46:22 -0500

    clang-format

--- cryptoFunctions.h
+++ cryptoFunctions.h
@@ -7,55 +7,54 @@
 
 void SHA256Hash(u8 *data, int dataLen, u8 *digest)
 {
-SHA256((unsigned char*)data, dataLen, (unsigned char*)digest);
-return;
+    SHA256((unsigned char *)data, dataLen, (unsigned char *)digest);
+    return;
 }
 
 void SHA1HMAC(u8 *chal, int chalLen, u8 *key, int keyLen, u8 *resp)
 {
-u32 respLen;
-HMAC(	EVP_sha1(),(unsigned char *)key,keyLen,(unsigned char *)chal,
-		chalLen,(unsigned char *)resp,&respLen);
-return;
+    u32 respLen;
+    HMAC(EVP_sha1(), (unsigned char *)key, keyLen, (unsigned char *)chal, chalLen, (unsigned char *)resp, &respLen);
+    return;
 }
 
 void decryptAES128ECB(u8 *cryptText, int cryptLen, u8 *key, u8 *plainText)
 {
-AES_KEY aeskey;
-AES_set_decrypt_key((unsigned char *)key, 128, &aeskey);
-AES_ecb_encrypt((unsigned char *)cryptText, (unsigned char *)plainText,
-					&aeskey, AES_DECRYPT);
-return;
+    AES_KEY aeskey;
+    AES_set_decrypt_key((unsigned char *)key, 128, &aeskey);
+    AES_ecb_encrypt((unsigned char *)cryptText, (unsigned char *)plainText, &aeskey, AES_DECRYPT);
+    return;
 }
 
 void decryptAES256CBC(u8 *cryptText, int cryptLen, u8 *key, u8 *IV, u8 *plainText)
 {
-AES_KEY aeskey;
-u8 tmpKey[32],tmpIV[16];
-/* Copy key and IV to temp arrays to preserve original information */
-memcpy(tmpKey,key,32);
-memcpy(tmpIV,IV,16);
-AES_set_decrypt_key((unsigned char *)tmpKey, 256, &aeskey);
-AES_cbc_encrypt((unsigned char *)cryptText, (unsigned char *)plainText, cryptLen,
-					&aeskey, (unsigned char *)tmpIV, AES_DECRYPT);
-return;
+    AES_KEY aeskey;
+    u8 tmpKey[32], tmpIV[16];
+    /* Copy key and IV to temp arrays to preserve original information */
+    memcpy(tmpKey, key, 32);
+    memcpy(tmpIV, IV, 16);
+    AES_set_decrypt_key((unsigned char *)tmpKey, 256, &aeskey);
+    AES_cbc_encrypt((unsigned char *)cryptText, (unsigned char *)plainText, cryptLen, &aeskey, (unsigned char *)tmpIV,
+                    AES_DECRYPT);
+    return;
 }
 
 u16 crc16(u8 *data, int dataLen)
 {
-int i,j,k;
-u16 crc=0xffff;
-for (i=0;i<dataLen;i++)
-	{
-	crc ^= data[i];
-	for (j=0;j<8;j++)
-		{
-		k=crc & 1;
-		crc >>= 1;
-		if (k) crc ^= 0x8408;
-		}
-	}
-return crc;
+    int i, j, k;
+    u16 crc = 0xffff;
+    for (i = 0; i < dataLen; i++)
+    {
+        crc ^= data[i];
+        for (j = 0; j < 8; j++)
+        {
+            k = crc & 1;
+            crc >>= 1;
+            if (k)
+                crc ^= 0x8408;
+        }
+    }
+    return crc;
 }
 
 #endif
--- globalDefinitions.h
+++ globalDefinitions.h
@@ -1,32 +1,35 @@
 #ifndef GLOBALDEFINITIONS_H
 #define GLOBALDEFINITIONS_H
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <signal.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdarg.h>
-#include <pwd.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
 #include <netdb.h>
-#include <time.h>
-#include <openssl/sha.h>
+#include <netinet/in.h>
 #include <openssl/aes.h>
 #include <openssl/hmac.h>
+#include <openssl/sha.h>
+#include <pwd.h>
 #include <signal.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #define MAX_PATH 4096
-#define MIN(A,B) ((A)<(B)?(A):(B))
-#define MAX(A,B) ((A)>(B)?(A):(B))
-#define BOUND(X,L,H) MAX(L,MIN(H,X))
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+#define BOUND(X, L, H) MAX(L, MIN(H, X))
 
-enum{FALSE,TRUE};
+enum
+{
+    FALSE,
+    TRUE
+};
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -47,59 +50,59 @@ typedef uint64_t u64;
 #define rwX 64
 
 /* Global Control Variables */
-char capdVersion[40]="1.03-dev";
-char passwdFile[MAX_PATH]="/etc/capd/capd.passwd";
-char counterFile[MAX_PATH]="/etc/capd/capd.counter";
-char logFile[MAX_PATH]="/var/log/capd.log";
-char jailPath[MAX_PATH]="/tmp/capd/";
-char openSSHPath[MAX_PATH]="/usr/sbin/openClose.sh";
-char serverAddress[MAX_NO_OF_SERVER_ADDR][32]={{0x00}};
+char capdVersion[40] = "1.03-dev";
+char passwdFile[MAX_PATH] = "/etc/capd/capd.passwd";
+char counterFile[MAX_PATH] = "/etc/capd/capd.counter";
+char logFile[MAX_PATH] = "/var/log/capd.log";
+char jailPath[MAX_PATH] = "/tmp/capd/";
+char openSSHPath[MAX_PATH] = "/usr/sbin/openClose.sh";
+char serverAddress[MAX_NO_OF_SERVER_ADDR][32] = {{0x00}};
 int noOfServerAddresses = 0;
-u32 deltaT = 30; /* Maximum allowed clock skew from client to server */
-u32 initTimeout = 5; /* Maximum time allowed to open local ssh connection */
+u32 deltaT = 30;       /* Maximum allowed clock skew from client to server */
+u32 initTimeout = 5;   /* Maximum time allowed to open local ssh connection */
 u32 spoofTimeout = 30; /* Maximum time allowed to open spoofed ssh connection */
-char user[32]="capd";
-int uid,gid;
-int port=62201;
-int verbosity=1;
+char user[32] = "capd";
+int uid, gid;
+int port = 62201;
+int verbosity = 1;
 
 /* Packet, Payload, and OTP Structures and Buffers */
 typedef struct
-  {
-  u8 authAddr[addrSize];
-  u8 connAddr[addrSize];
-  u8 serverAddr[addrSize];
-  u8 OTP[OTPSize];
-  char username[32];
-  u8 entropy[32];
-  u8 chksum[chksumSize];
-  } plain_t;
+{
+    u8 authAddr[addrSize];
+    u8 connAddr[addrSize];
+    u8 serverAddr[addrSize];
+    u8 OTP[OTPSize];
+    char username[32];
+    u8 entropy[32];
+    u8 chksum[chksumSize];
+} plain_t;
 u8 plainBuffer[sizeof(plain_t)];
 plain_t *plain = (plain_t *)plainBuffer;
-#define chksumBlockSize (sizeof(plain_t)-chksumSize)
+#define chksumBlockSize (sizeof(plain_t) - chksumSize)
 
 typedef struct
-  {
-  int timestamp;
-  int serial;
-  u8 IV[16];
-  u8 challenge[32];
-  u8 encBlock[sizeof(plain_t)];
-  u8 MAC[32];
-  } packet_t;
+{
+    int timestamp;
+    int serial;
+    u8 IV[16];
+    u8 challenge[32];
+    u8 encBlock[sizeof(plain_t)];
+    u8 MAC[32];
+} packet_t;
 
-packet_t *packet=NULL;
+packet_t *packet = NULL;
 
-#define MACBlockSize (sizeof(packet_t)-MACSize)
+#define MACBlockSize (sizeof(packet_t) - MACSize)
 
 typedef struct
-  {
-  u8 OTPChallenge[6];
-  u16 insertCounter;
-  u8 timestamp[3];
-  u8 sessionCounter;
-  u16 random;
-  u16 crc;
-  } OTP_t;
+{
+    u8 OTPChallenge[6];
+    u16 insertCounter;
+    u8 timestamp[3];
+    u8 sessionCounter;
+    u16 random;
+    u16 crc;
+} OTP_t;
 
 #endif
