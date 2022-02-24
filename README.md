3e0731eb5ef8c3a0bd9e144ce0325cf918899f06
Mark Meredith <mark.meredith@netl.doe.gov>
2022-02-24 13:14:27 -0500

    Add tests

--- README.md
+++ README.md
@@ -3,24 +3,24 @@
 The `capd` service listen for CAP packets, and authenticates them to determine
 whether access through the firewall should be permitted.
 
-# Build Requirements
+## Build Requirements
 
  - GCC
  - OpenSSL
 
 For instance, on Ubuntu: `apt install build-essential libssl-dev`
 
-# Build
+## Build
 ```
 ./build.sh
 ```
 
-# Run
+## Run
 ```
 ./build/capd
 ```
 
-# Development
+## Development
 
 To run tests, build with Meson:
 
@@ -29,4 +29,42 @@ pip install meson ninja
 meson build
 meson compile -C build
 meson test -C build
+```
+
+# mfa_program tool for programming Yubikey
+
+
+## Install dependencies
+The `mfa_program.py` script requires `pyOpenSSL` to run.
+
+```
+pip install pyOpenSSL
+```
+
+To create certificate:
+
+```
+./mfa_program.py ca
+```
+
+To program a yubikey:
+
+```
+./mfa_program.py yk
+```
+
+## Development
+
+The pytests require a Yubikey to run.
+
+*DO NOT RUN THE TESTS ON A PRODUCTION YUBIKEY.*
+
+This  Yubikey will be reset and wiped out.
+
+To run tests:
+
+```
+pip install poetry
+poetry install
+poetry run python -m pytest --mypy
 ```
---
+++ pytest/test_mfa_program.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from unittest.mock import patch
+
+import mfa_program
+
+
+def test_default():
+    mfa_program.main([])
+
+
+@patch("mfa_program.input")
+@patch("mfa_program.getpass")
+def test_make_cert_default(mock_getpass, mock_input, tmpdir):
+    mock_getpass.getpass.return_value = "yes"
+    mock_input.return_value = "yes"
+    # p = tmpdir.mkdir()
+    default_cert = tmpdir / "CACertificate.pem"
+    default_key = tmpdir / "CAPrivateKey.pem"
+    assert not default_cert.exists()
+    assert not default_key.exists()
+
+    with tmpdir.as_cwd() as cwd:
+        mfa_program.main(["ca"])
+
+    assert default_cert.exists()
+    assert default_key.exists()
+
+
+@patch("mfa_program.input")
+@patch("mfa_program.getpass")
+def test_make_cert_cancel(mock_getpass, mock_input, tmpdir):
+    mock_getpass.getpass.return_value = "no"
+    mock_input.return_value = "no"
+    default_cert = Path(tmpdir / "CACertificate.pem")
+    default_key = Path(tmpdir / "CAPrivateKey.pem")
+    default_cert.touch()
+    default_key.touch()
+    assert not default_cert.stat().st_size
+    assert not default_key.stat().st_size
+
+    with tmpdir.as_cwd() as cwd:
+        mfa_program.main(["ca"])
+
+    assert not default_cert.stat().st_size
+    assert not default_key.stat().st_size
+
+
+@patch("mfa_program.input")
+@patch("mfa_program.getpass")
+def test_make_cert_overwrite(mock_getpass, mock_input, tmpdir):
+    mock_getpass.getpass.return_value = "yes"
+    mock_input.return_value = "yes"
+    default_cert = Path(tmpdir / "CACertificate.pem")
+    default_key = Path(tmpdir / "CAPrivateKey.pem")
+    default_cert.touch()
+    default_key.touch()
+    assert not default_cert.stat().st_size
+    assert not default_key.stat().st_size
+
+    with tmpdir.as_cwd() as cwd:
+        mfa_program.main(["ca"])
+
+    assert default_cert.stat().st_size
+    assert default_key.stat().st_size
+
+
+@patch("mfa_program.input")
+@patch("mfa_program.getpass")
+def test_make_cert_custom(mock_getpass, mock_input, tmpdir):
+    mock_getpass.getpass.return_value = "meh"
+    mock_input.return_value = "meh"
+    cert = tmpdir / "my_ca.pem"
+    key = tmpdir / "my_key.pem"
+    assert not cert.exists()
+    assert not key.exists()
+
+    with tmpdir.as_cwd() as cwd:
+        mfa_program.main(["ca", "-t", "my_ca.pem", "-p", "my_key.pem"])
+
+    assert cert.exists()
+    assert key.exists()
+
+
+@patch("mfa_program.input")
+@patch("mfa_program.getpass")
+def test_program_yubikey(mock_getpass, mock_input, tmpdir):
+    mock_getpass.getpass.return_value = "meh"
+    mock_input.return_value = "meh"
+
+    cert = tmpdir / "CACertificate.pem"
+    key = tmpdir / "CAPrivateKey.pem"
+    assert not cert.exists()
+    assert not key.exists()
+
+    with tmpdir.as_cwd() as cwd:
+        mfa_program.main(["ca"])
+        assert cert.exists()
+        assert key.exists()
+
+        mfa_program.main(["yk", "--user", "the_user", "--once"])
