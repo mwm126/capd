1e4e75401d1863b166da30cd112f4039dd43ef54
Mark Meredith <mark.meredith@netl.doe.gov>
2022-02-23 21:04:06 -0500

    Refactor

--- mfa_program.py
+++ mfa_program.py
@@ -9,156 +9,37 @@ import subprocess
 import sys
 import time
 
+from pathlib import Path
 from typing import Tuple
 
 from OpenSSL import crypto
 
 # Global Variables
-storagePath = "/mnt/mfa/"
-ykinfo = "/usr/local/bin/ykinfo"
-ykpersonalize = "/usr/local/bin/ykpersonalize"
-ykchalresp = "/usr/local/bin/ykchalresp"
-yubicoPivTool = "/usr/local/bin/yubico-piv-tool"
+storagePath = Path(".")
 
 # secrets
 hashIDSalt = "4FEGy8Vb2ih8fRZow2qK2QE32BYe0ZffTtEmj1QI0sJ5l7HOMPcm4G+F"
-hmacGenTxt = "02VDv98CjuUVnjDOv1ySVB+YbEFISOUUW6Lgc2UGCjuSQJHzRhMLfKP3vFkMikSp"
-otpGenTxt = "EdnkfUYMK8yl1XxJPibr60g6laGvpk7tBPVMCCyxpam5dQyziBx7K5DdraA2c6KC"
-hmacGenerator = base64.b64decode(hmacGenTxt)
-otpGenerator = base64.b64decode(otpGenTxt)
-pinGenTxt = "bseTuIXxT5E+Z7zKZlHnRzmv8J5/UY315O08z+N/LL7Z1LRku9ocQJbKjYgNMURz"
-pinGenerator = base64.b64decode(pinGenTxt)
-pukGenTxt = "/7jZypeK4aQqBFZvecVrgRep+ZBIEiu4TQP4lQtamYbdV0UJgvaEMsTYOG4bylX4"
-pukGenerator = base64.b64decode(pukGenTxt)
+hmacGenerator = base64.b64decode("02VDv98CjuUVnjDOv1ySVB+YbEFISOUUW6Lgc2UGCjuSQJHzRhMLfKP3vFkMikSp")
+otpGenerator = base64.b64decode("EdnkfUYMK8yl1XxJPibr60g6laGvpk7tBPVMCCyxpam5dQyziBx7K5DdraA2c6KC")
+pinGenerator = base64.b64decode("bseTuIXxT5E+Z7zKZlHnRzmv8J5/UY315O08z+N/LL7Z1LRku9ocQJbKjYgNMURz")
+pukGenerator = base64.b64decode("/7jZypeK4aQqBFZvecVrgRep+ZBIEiu4TQP4lQtamYbdV0UJgvaEMsTYOG4bylX4")
 mfaAccess = "83b9fe94a405"
 MGMKEY = "e1a055640fd1b0ef5eb5b9dfd5c867d3e035bf2885ca5276"
 
 
-def main():
+def main(args: list[str]):
     print("\nMFA4 SciLAN/HPC Programmer\n")
 
-    if len(sys.argv) == 2 and sys.argv[1] == "-c":
+    if len(args) == 2 and args[1] == "-c":
         create_key_pair()
     else:
         program_user_key()
 
 
-def sha256(string: str) -> bytes:
-    m = hashlib.sha256()
-    m.update(string.encode("utf-8"))
-    return m.digest()
-
-
-def sha1(string: bytes) -> bytes:
-    m = hashlib.sha1()
-    m.update(string)
-    return m.digest()
-
-
-def run(cmd: str, args: str) -> Tuple[bytes, bytes]:
-    argList = [cmd] + args.split()
-    with subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
-        return p.communicate()
-
-
-def pivTool(args: str) -> Tuple[bytes, bytes]:
-    return run(yubicoPivTool, args)
-
-
-def ykPers(args: str) -> Tuple[bytes, bytes]:
-    return run(ykpersonalize, args)
-
-
-def ykChalResp(args: str):
-    return run(ykchalresp, args)
-
-
-def programRSASlot(
-    mgmKey: str,
-    username: str,
-    keyPIN: str,
-    slot: str,
-    serial: int,
-    CACert: crypto.X509,
-    CAPrivateKey: crypto.PKey,
-) -> None:
-    # make a new public key for the user
-    userKeyPair = crypto.PKey()
-    userKeyPair.generate_key(crypto.TYPE_RSA, 2048)
-
-    userCert = crypto.X509()
-    userCert.set_serial_number(serial)
-    userCert.gmtime_adj_notBefore(0)
-    userCert.gmtime_adj_notAfter(50 * 365 * 24 * 60 * 60)
-    userCert.get_subject().C = "US"
-    userCert.get_subject().O = "USDOE-NETL"
-    userCert.get_subject().CN = username
-    userCert.set_pubkey(userKeyPair)
-    userCert.set_issuer(CACert.get_subject())
-    userCert.sign(CAPrivateKey, "sha512")
-
-    # verify user cert
-    store = crypto.X509Store()
-    store.add_cert(CACert)
-    store_ctx = crypto.X509StoreContext(store, userCert)
-    try:
-        store_ctx.verify_certificate()
-    except ValueError:
-        print("User Certificate verification failure.  Exiting.")
-        sys.exit()
-    print("User Certificate for slot " + slot + " created and verified.")
-
-    print("Pushing user certificate to yubikey, slot " + slot + ".")
-    userCertPem = crypto.dump_certificate(crypto.FILETYPE_PEM, userCert)
-    result = subprocess.run(
-        [yubicoPivTool, "-k", mgmKey, "-s", slot, "-a", "import-certificate"],
-        input=userCertPem,
-        check=True,
-    )
-    print(result.stdout)
-
-    result = subprocess.run(
-        [yubicoPivTool, "-s", slot, "-a", "read-certificate"],
-        check=True,
-    )
-    userCertPem2 = result.stdout
-
-    if userCertPem != userCertPem2:
-        print("User Certificate on key not verified.  Exiting.")
-        sys.exit()
-    print("User Certificate on key is verified.")
-
-    print("Pushing user private key to yubikey, slot " + slot + ".")
-    result = subprocess.run(
-        [yubicoPivTool, "-k" + mgmKey, "-s", slot, "-a", "import-key"],
-        input=crypto.dump_privatekey(crypto.FILETYPE_PEM, userKeyPair),
-        check=True,
-    )
-    print(result.stdout)
-
-    print("Verifying RSA2048 signature with key in slot " + slot + ".")
-    result = subprocess.run(
-        [
-            yubicoPivTool,
-            "-a",
-            "verify-pin",
-            "-P",
-            keyPIN,
-            "-s",
-            slot,
-            "-a",
-            "test-signature",
-        ],
-        input=userCertPem2,
-        check=True,
-    )
-    print(result.stdout)
-
-
 def create_key_pair():
     """Handle Key Pair Creation"""
     print("Preparing to create a new Certificate Authority key pair.")
-    print("This will overwrite any existing keys in " + storagePath + ".")
+    print(f"This will overwrite any existing keys in {storagePath}.")
     yOrN = input("Do you want to continue? ")
     if yOrN.lower() not in ["y", "yes"]:
         print("\nNo file changes made.  Exiting.\n")
@@ -169,8 +50,7 @@ def create_key_pair():
 
     while True:
         privatePass = getpass.getpass("\nEnter passphrase for CA Private Key: ")
-        privatePass2 = getpass.getpass("Re-enter passphrase: ")
-        if privatePass == privatePass2:
+        if privatePass == getpass.getpass("Re-enter passphrase: "):
             break
         print("Passphases do not match.")
 
@@ -199,20 +79,23 @@ def create_key_pair():
         sys.exit()
     print("CA Certificate verification completed.")
 
-    print("\nWriting CA Certificate to " + storagePath + "CACertificate.pem.")
-    with open(storagePath + "CACertificate.pem", "wb") as CACertFile:
+    ca_cert_pem = storagePath / "CACertificate.pem"
+    print(f"\nWriting CA Certificate to {ca_cert_pem}.")
+    with open(ca_cert_pem, "wb") as CACertFile:
         CACertFile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, CACert))
 
-    print("Writing Encrypted CA Private Key to " + storagePath + "CAPrivateKey.pem.")
-    with open(storagePath + "CAPrivateKey.pem", "wb") as CAPrivateKeyFile:
-        CAPrivateKeyFile.write(
-            crypto.dump_privatekey(
-                crypto.FILETYPE_PEM,
-                CAKeyPair,
-                "aes-256-cbc",
-                bytes(privatePass, "utf-8"),
-            )
-        )
+    ca_priv_pem = storagePath / "CAPrivateKey.pem"
+    print(f"Writing Encrypted CA Private Key to {ca_priv_pem}.")
+
+    priv_key = crypto.dump_privatekey(
+        crypto.FILETYPE_PEM,
+        CAKeyPair,
+        "aes-256-cbc",
+        bytes(privatePass, "utf-8"),
+    )
+
+    with open(ca_priv_pem, "wb") as CAPrivateKeyFile:
+        CAPrivateKeyFile.write(priv_key)
     print("Exiting.\n")
 
 
@@ -222,7 +105,7 @@ def program_user_key():
     # Load and decrypt CA certificate and private key
     privatePass = getpass.getpass("Enter passphrase for CA Private Key: ")
     try:
-        with open(storagePath + "CAPrivateKey.pem", "rb") as CAPrivateKeyFile:
+        with open(storagePath / "CAPrivateKey.pem", "rb") as CAPrivateKeyFile:
             CAPrivateKey = crypto.load_privatekey(
                 crypto.FILETYPE_PEM,
                 CAPrivateKeyFile.read(),
@@ -235,7 +118,7 @@ def program_user_key():
         print("Exiting.")
         sys.exit()
     try:
-        with open(storagePath + "CACertificate.pem", "rb") as CACertFile:
+        with open(storagePath / "CACertificate.pem", "rb") as CACertFile:
             CACert = crypto.load_certificate(crypto.FILETYPE_PEM, CACertFile.read())
     except ValueError:
         print("Error opening CA Certificate.")
@@ -255,7 +138,7 @@ def program_user_key():
 
         # get serial number from yubikey
         try:
-            serialText = subprocess.check_output([ykinfo, "-sq"])
+            serialText = subprocess.check_output(["ykinfo", "-sq"])
             serial = int(serialText)
         except ValueError:
             print("Problem detecting Yubikey.")
@@ -310,8 +193,9 @@ def program_user_key():
         chal = os.urandom(63)
         keyResp, _ = ykChalResp("-2 -H -x " + chal.hex())
         h = hmac.new(userHmac, chal, hashlib.sha1)
-        chkResp = h.digest().hex()
+        chkResp = h.digest().hex().encode("utf-8")
         if keyResp.strip() != chkResp:
+            print(keyResp.strip(), chkResp)
             print("Slot 2 HMAC test failure.  Exiting.")
             sys.exit()
         print("HMAC Slot 2 test complete.")
@@ -321,14 +205,13 @@ def program_user_key():
         keyPUK = base64.b64encode(sha256(str(serial) + str(pukGenerator))).decode("utf-8")[0:8]
         print("\nSetting yubikey configuration data.")
         try:
-            print(pivTool("-a set-chuid")[1])
-            print(pivTool("-a set-ccc")[1])
-            print(pivTool("-a set-mgm-key -n" + MGMKEY)[1])
-            print(pivTool("-a change-pin -P 123456 -N" + keyPIN)[1])
-            print(pivTool("-a change-puk -P 12345678 -N" + keyPUK)[1])
+            print(pivTool("-a set-chuid"))
+            print(pivTool("-a set-ccc"))
+            print(pivTool("-a set-mgm-key -n" + MGMKEY))
+            print(pivTool("-a change-pin -P 123456 -N" + keyPIN))
+            print(pivTool("-a change-puk -P 12345678 -N" + keyPUK))
         except ValueError:
             sys.exit()
-        print()
 
         # Program all three PIV certificate slots
         programRSASlot(MGMKEY, username, keyPIN, "9a", serial, CACert, CAPrivateKey)
@@ -339,7 +222,7 @@ def program_user_key():
         print("HMAC Secret: " + userHmac.hex())
 
         # Write out to log file
-        with open(storagePath + "mfaKey.log", "a", encoding="utf-8") as f:
+        with open(storagePath / "mfaKey.log", "a", encoding="utf-8") as f:
             out = "[" + time.asctime(time.localtime(time.time())) + "]   "
             out += username + "\t"
             out += str(serial) + "   "
@@ -349,5 +232,116 @@ def program_user_key():
         print("Key programmed and tested for user " + username + ".  Remove key.")
 
 
+def sha256(string: str) -> bytes:
+    m = hashlib.sha256()
+    m.update(string.encode("utf-8"))
+    return m.digest()
+
+
+def sha1(string: bytes) -> bytes:
+    m = hashlib.sha1()
+    m.update(string)
+    return m.digest()
+
+
+def run(cmd: str, args: str) -> Tuple[bytes, bytes]:
+    argList = [cmd] + args.split()
+    with subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
+        return p.communicate()
+
+
+def pivTool(args: str) -> str:
+    return run("yubico-piv-tool", args)[1].decode("utf-8")
+
+
+def ykPers(args: str) -> Tuple[bytes, bytes]:
+    return run("ykpersonalize", args)
+
+
+def ykChalResp(args: str):
+    return run("ykchalresp", args)
+
+
+def programRSASlot(
+    mgmKey: str,
+    username: str,
+    keyPIN: str,
+    slot: str,
+    serial: int,
+    CACert: crypto.X509,
+    CAPrivateKey: crypto.PKey,
+) -> None:
+    # make a new public key for the user
+    userKeyPair = crypto.PKey()
+    userKeyPair.generate_key(crypto.TYPE_RSA, 2048)
+
+    userCert = crypto.X509()
+    userCert.set_serial_number(serial)
+    userCert.gmtime_adj_notBefore(0)
+    userCert.gmtime_adj_notAfter(50 * 365 * 24 * 60 * 60)
+    userCert.get_subject().C = "US"
+    userCert.get_subject().O = "USDOE-NETL"
+    userCert.get_subject().CN = username
+    userCert.set_pubkey(userKeyPair)
+    userCert.set_issuer(CACert.get_subject())
+    userCert.sign(CAPrivateKey, "sha512")
+
+    # verify user cert
+    store = crypto.X509Store()
+    store.add_cert(CACert)
+    store_ctx = crypto.X509StoreContext(store, userCert)
+    try:
+        store_ctx.verify_certificate()
+    except ValueError:
+        print("User Certificate verification failure.  Exiting.")
+        sys.exit()
+    print("User Certificate for slot " + slot + " created and verified.")
+
+    print("Pushing user certificate to yubikey, slot " + slot + ".")
+    userCertPem = crypto.dump_certificate(crypto.FILETYPE_PEM, userCert).decode("utf-8")
+    import_cert_out = subprocess.check_output(
+        ["yubico-piv-tool", f"--key={mgmKey}", "-s", slot, "-a", "import-certificate"],
+        encoding="utf-8",
+        input=userCertPem,
+    )
+    print(import_cert_out)
+
+    userCertPem2 = subprocess.check_output(
+        ["yubico-piv-tool", "-s", slot, "-a", "read-certificate"], encoding="utf-8"
+    )
+
+    if userCertPem != userCertPem2:
+        print("User Certificate on key not verified.  Exiting.")
+        sys.exit()
+    print("User Certificate on key is verified.")
+
+    print("Pushing user private key to yubikey, slot " + slot + ".")
+    priv_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, userKeyPair).decode("utf-8")
+    import_out = subprocess.check_output(
+        ["yubico-piv-tool", f"--key={mgmKey}", "-s", slot, "-a", "import-key"],
+        input=priv_key,
+        encoding="utf-8",
+    )
+    print(import_out)
+
+    print("Verifying RSA2048 signature with key in slot " + slot + ".")
+    verify_pin_out = subprocess.check_output(
+        [
+            "yubico-piv-tool",
+            "-a",
+            "verify-pin",
+            "-P",
+            keyPIN,
+            "-s",
+            slot,
+            "-a",
+            "test-signature",
+        ],
+        input=userCertPem2,
+        encoding="utf-8",
+    )
+    print(verify_pin_out)
+
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
