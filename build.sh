965e77065dcec6edef1e59e91e6872a79c44474d
Mark Meredith <mark.meredith@netl.doe.gov>
2022-02-27 21:15:15 -0500

    Revert "shellcheck"
    This reverts commit bfde1c2c23525b0d485a74896c749dc8d57c323b.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcc -o capd src/*.c \
-    -lcrypto -lrt -std=c2x -Wall -O3 -D_DEFAULT_SOURCE -DCAPD_VERSION="$(cat VERSION)"
+    -lcrypto -lrt -std=c2x -Wall -O3 -D_DEFAULT_SOURCE -DCAPD_VERSION=$(cat VERSION)
