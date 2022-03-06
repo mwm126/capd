05d0747d107122d7ef8991ac85173286e225ce70
Mark Meredith <mark.meredith@netl.doe.gov>
2022-03-06 09:21:15 -0500

    wip

--- packaging/pkg_deb.sh
+++ packaging/pkg_deb.sh
@@ -9,17 +9,25 @@ PKG_DIR=$(dirname "$0")
 
 TAG=$(cat "$PKG_DIR"/../VERSION)
 
-CAPD_DEB=capd_inst.deb
+CAPD_DEB=capd_22.2.17-1_amd64.deb
 SRC_TAR=build/meson-dist/capd-${TAG}.tar.xz
 
-rm -rf ${build_src}
 meson ${build_src}
-meson dist -C ${build_src}
+meson dist -C ${build_src} --include-subprojects
+PKG_NAME=capd_22.2.17
+SRC_TAR=capd-${TAG}.tar.xz
+cp "${build_src}/meson-dist/capd-${TAG}.tar.xz" .
 
 cd "$PKG_DIR"/..
 
 # Build
-env DOCKER_BUILDKIT=1 docker build -f "$PKG_DIR"/Dockerfile-deb -t ${BUILD_IMG} --build-arg SRC_TAR="${SRC_TAR}" .
+export DOCKER_BUILDKIT=0
+docker build \
+       -f "$PKG_DIR"/Dockerfile-deb \
+       -t ${BUILD_IMG} \
+       --build-arg SRC_TAR="${SRC_TAR}" \
+       --build-arg PKG_NAME="${PKG_NAME}" \
+       .
 
 # Copy .deb to host/
 docker run --rm -v "$PWD":/host --user "$( id -u ):$( id -g )" ${BUILD_IMG} /bin/sh -c "cp /${CAPD_DEB} /host"
