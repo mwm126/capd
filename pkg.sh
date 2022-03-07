9823b8315c52a5041a66f234466f7cff49e65b8d
Mark Meredith <mark.meredith@netl.doe.gov>
2022-03-07 08:28:02 -0500

    reorg

--- pkg.sh
+++ pkg.sh
@@ -3,66 +3,86 @@
 IFS=$'\n\t'
 set -xeou pipefail
 
+_topdir=$(dirname "$0")
+TOPDIR=$(realpath "$_topdir")
+TAG=$(cat "$_topdir"/VERSION)
+VERSION=${TAG:1}
+
+GH_TITLE="CAP daemon Release ${TAG}"
+GH_NOTES="Build RPM and DPKG"
+
 build_src=build-src
-BUILD_DEB_IMG=capd_deb_img
-BUILD_RPM_IMG=capd_rpm_img
-PKG_DIR=$(dirname "$0")
 
-TAG=$(cat "$PKG_DIR"/VERSION)
+SRC_TAR=${build_src}/meson-dist/capd-${TAG}.tar.xz
+export DOCKER_BUILDKIT=1
 
-CAPD_DEB=capd_22.2.17-1_amd64.deb
-CAPD_RPM=capd-${TAG}-1.x86_64.rpm
-SRC_TAR=build/meson-dist/capd-${TAG}.tar.xz
 
-function build_src() {
-    meson ${build_src}
-    meson dist -C ${build_src} --include-subprojects
-    PKG_NAME=capd_22.2.17
-    SRC_TAR=capd-${TAG}.tar.xz
-    cp "${build_src}/meson-dist/capd-${TAG}.tar.xz" .
+function main() {
+    if [ "$1" == "deb" ]; then
+        CAPD_DEB=capd_${VERSION}-1_amd64.deb
+        CAPD_PKG="${CAPD_DEB}"
+        DOCKER_IMG=capd_deb_img
+        DOCKER_DIR="$TOPDIR"/.docker/deb
+        CAPD_PKG_PATH="/workdir/${CAPD_DEB}"
+    elif [ "$1" == "rpm" ]; then
+        CAPD_RPM=capd-${TAG}-1.x86_64.rpm
+        CAPD_PKG="${CAPD_RPM}"
+        DOCKER_IMG=capd_rpm_img
+        DOCKER_DIR="$TOPDIR"/.docker/rpm
+        CAPD_PKG_PATH="/root/rpmbuild/RPMS/x86_64/${CAPD_RPM}"
+    else
+        echo "Usage: ./pkg.sh rpm      ot ./pkg.sh deb"
+        exit 0
+    fi
+
+    cd "$TOPDIR"
+    build_pkg
+    test_pkg "$1"
+
+    if [ "${2:-}" == "upload" ]; then
+        github_upload "${CAPD_PKG}"
+    else
+        echo
+        echo "Build successful. Run again as './pkg.sh upload' to upload assets to Github."
+    fi
 }
 
 
-function build_rpm() {
+function build_pkg() {
+    meson "${build_src}"
+    meson dist -C "${build_src}" --include-subprojects
 
-    # Build
-    export DOCKER_BUILDKIT=0
-    docker build -f "$PKG_DIR"/Dockerfile-rpm -t ${BUILD_RPM_IMG} --build-arg SRC_TAR="${SRC_TAR}" .
-
-    # Copy .rpm to host/
-    docker run --rm -v "$PWD":/host ${BUILD_RPM_IMG} /bin/sh -c "cp /root/rpmbuild/RPMS/x86_64/${CAPD_RPM} /host"
-
-    # Test
-    docker run --rm -v "$PWD":/host amazonlinux:2022 /bin/sh -c "rpm -i /host/${CAPD_RPM}; rpm -ql capd"
-}
-
-function build_deb() {
-
-    # Build
-    export DOCKER_BUILDKIT=0
     docker build \
-           -f "$PKG_DIR"/Dockerfile-deb \
-           -t ${BUILD_DEB_IMG} \
+           -t ${DOCKER_IMG} \
            --build-arg SRC_TAR="${SRC_TAR}" \
-           --build-arg PKG_NAME="${PKG_NAME}" \
-           .
+           -f "$DOCKER_DIR/Dockerfile" .
 
-    # Copy .deb to host/
-    docker run --rm -v "$PWD":/host --user "$( id -u ):$( id -g )" ${BUILD_DEB_IMG} /bin/sh -c "cp ../${CAPD_DEB} /host"
+    docker run --rm -v "$PWD":/host \
+           /bin/sh -c "cp ${CAPD_PKG_PATH} /host" \
+           ${DOCKER_IMG}
+}
 
-    # Test
-    docker run --rm -v "$PWD":/host ubuntu:18.04 /bin/sh -c "apt update; apt install -y /host/${CAPD_DEB}; dpkg -L capd"
-};
 
-if [ "$1" == "deb" ]; then
-    cd "$PKG_DIR"
-    build_src
-    build_deb
-elif [ "$1" == "rpm" ]; then
-    cd "$PKG_DIR"
-    build_src
-    build_rpm
-else
-    echo "Usage: ./pkg.sh rpm      ot ./pkg.sh deb"
+function test_pkg {
+    if [ "$1" == "deb" ]; then
+        docker run --rm -v "$PWD":/host \
+               /bin/sh -c "apt update; apt install -y /host/${CAPD_DEB}; dpkg -L capd" \
+               ubuntu:18.04
 
-fi
+    elif [ "$1" == "rpm" ]; then
+        docker run --rm -v "$PWD":/host \
+               /bin/sh -c "rpm -i /host/${CAPD_RPM}; rpm -ql capd" \
+               amazonlinux:2022
+    fi
+}
+
+
+function github_upload {
+    gh release create "${TAG}" \
+       --draft \
+       -n "${GH_NOTES}" -t "${GH_TITLE}" \
+       "$1"
+}
+
+
+main "$@"
